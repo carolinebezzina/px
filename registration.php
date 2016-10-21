1dc343e5f9665fb4e6daa9d65a2ac01be5e4f065
Caroline Bezzina <c_bezzina@hotmail.com>
2016-10-22 10:58:41 +1100

    fixed layout of registration page

--- registration.php
+++ registration.php
@@ -1,5 +1,5 @@
 <?php
-	$WebsiteRoot = $_SERVER['DOCUMENT_ROOT'];	include($WebsiteRoot . '/includes/validate.php');	include($WebsiteRoot . '/includes/newUser.php');
+	$WebsiteRoot = $_SERVER['DOCUMENT_ROOT'];	include($WebsiteRoot . '/includes/validate.php');	include($WebsiteRoot . '/includes/newUser.php');		if(isset($_POST['submit'])){				$state = $_POST['State-req'];					switch ($state){				case "New South Wales":					$NSW = 'selected="selected"';					break;				case "Australian Capital Territory":					$ACT = 'selected="selected"';					break;				case "Queensland":					$QLD = 'selected="selected"';					break;				case "Northern Territory":					$NT = 'selected="selected"';					break;				case "Tasmania":					$TAS = 'selected="selected"';					break;				case "Victoria":					$VIC = 'selected="selected"';					break;				case "South Australia":					$SA = 'selected="selected"';					break;				case "Weatern Australia":					$WA = 'selected="selected"';					break;		}	}	
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -96,7 +96,7 @@
                     <a class="left carousel-control" data-slide="prev" href="#myCarousel" role="button"><span aria-hidden="true" class="glyphicon glyphicon-chevron-left"></span> <span class="sr-only">Previous</span></a> <a class="right carousel-control" data-slide="next" href= "#myCarousel" role="button"><span aria-hidden="true" class="glyphicon glyphicon-chevron-right"></span> <span class=                   "sr-only">Next</span></a>
                 </div>
                 <div class="mainContent">
-					<h1>Customer Registration</h1>
+					<h1> Customer Registration</h1>
 					<?php 
 					if(isset($messages)){
 						foreach($messages as $key => $value){
@@ -114,24 +114,24 @@
                                 <span class = "labels">Full Name:</span>
                                 <div class="inputs">
                                     <div class="left-column">
-                                        <input type="text" name="First_Name-req-alpha" id="First_Name-req-alpha" maxlength="40"/><span class="required"><?php if(isset($reqfirst)){echo htmlentities($reqfirst); }?></span>
+                                        <input type="text" name="First_Name-req-alpha" id="First_Name-req-alpha"value="<?php if(isset($_POST['First_Name-req-alpha'])) echo htmlentities($_POST['First_Name-req-alpha']);?>"  maxlength="40"/><span class="required"></span>
                                         <br/><label>First Name</label>
                                     </div>
                                     <div class="right-column">
-                                        <input type="text" name="Last_Name-req-alpha" id="lName" maxlength="40"/><span class="required"><?php if(isset($reqLast)){echo htmlentities($reqLast); }?></span>
+                                        <input type="text" name="Last_Name-req-alpha" id="lName" value="<?php if(isset($_POST['Last_Name-req-alpha'])) echo htmlentities($_POST['Last_Name-req-alpha']);?>" maxlength="40"/><span class="required"></span>
                                         <br/><label>Last Name</label>
                                     </div>
                                 </div>
                             </li>
 							<li>
                                 <span class = "labels">Company Name:</span>
-                                <div class="inputs"><input type="text" name="Company_Name-req-alphachar" id="cName" maxlength="50"/><span class="required"><?php if(isset($reqCompany)){echo htmlentities($reqCompany); }?></span><label class="visible-xs">Company Name</label></div>
+                                <div class="inputs"><input type="text" name="Company_Name-req-alphachar" id="cName" maxlength="50" value="<?php if(isset($_POST['Company_Name-req-alphachar'])) echo htmlentities($_POST['Company_Name-req-alphachar']);?>"/><span class="required"></span><label class="visible-xs">Company Name</label></div>
                             </li>
 							<li>
                                 <span class="labels">Email Address:</span>
                                 <div class="inputs">
                                     <div class="left-column">
-                                        <input type="email" name="Email-req-email" id="eMail" maxlength="50"/><span class="required"><?php if(isset($reqEmail)){echo htmlentities($reqEmail); }?></span><label class="visible-xs">Email Address</label>
+                                        <input type="email" name="Email-req-email" id="eMail" maxlength="50" value="<?php if(isset($_POST['Email-req-email'])) echo htmlentities($_POST['Email-req-email']);?>" /><span class="required"></span><label class="visible-xs">Email Address</label>
                                     </div>
                                     <div class="right-column">
                                         <label>Note: Email is also your username!</label></div>
@@ -139,31 +139,34 @@
                             </li>
 							<li>
                                 <span class="labels">Phone Number:</span>
-                                <div class="inputs"><input type="text" name="Phone-req-num" id="phone" maxlength="12"/><span class="required"><?php if(isset($reqPhone)){echo htmlentities($reqPhone); }?></span><label class = "visible-xs">Phone Number</label></div>
+                                <div class="inputs"><input type="text" name="Phone-req-num" id="phone" maxlength="12" value="<?php if(isset($_POST['Phone-req-num'])) echo htmlentities($_POST['Phone-req-num']);?>"/><span class="required"></span><label class = "visible-xs">Phone Number</label></div>
                             </li>
-							<li>
+							<li>						
                                 <span class="labels">Address:</span>
                                 <div class="inputs">
                                     <div class="left-column">
-                                        <input type="text" name="Address-req-alphachar" id="address" value="<?php  ?>" maxlength="80"/><span class="required"><?php if(isset($reqAddress)){echo htmlentities($reqAddress); }?></span>
+                                        <input type="text" name="Address-req-alphachar" id="address"  value="<?php if(isset($_POST['Address-req-address'])) echo htmlentities($_POST['Address-req-address']);?>" maxlength="80"/><span class="required"></span>
                                         <br/><label>Address</label>
                                         <br/>
-                                        <select name="State-req" id="state">
+                                        <select name="State-req" id="State-req" >
                                             <option name="select" value="selectreq" >-------------Select-------------</option>
-                                            <option name="act">Australian Capital Territory</option>
-                                            <option name="nsw">New South Wales</option>
-                                            <option name="nt">Northern Territory</option>
-                                            <option name="qld">Queensland</option>
-                                            <option name="sa">South Australia</option>
-                                            <option name="tas">Tasmania</option>
-                                            <option name="vic">Victoria</option>
-                                            <option name="wa">Western Australia</option>
+                                            <option name="act" value="Australian Capital Territory" <?php echo $ACT; ?> >Australian Capital Territory</option>
+                                            <option name="nsw" value="New South Wales"<?php echo $NSW; ?>>New South Wales</option>
+                                            <option name="nt" value="Northern Territory" <?php echo $NT; ?>>Northern Territory</option>
+                                            <option name="qld" value="Queensland" <?php echo $QLD; ?>>Queensland</option>
+                                            <option name="sa" value="South Australia" <?php echo $SA; ?>>South Australia</option>
+                                            <option name="tas" value="Tasmania" <?php echo $TAS; ?>>Tasmania</option>
+                                            <option name="vic" value="Victoria" <?php echo $VIC; ?>>Victoria</option>
+                                            <option name="wa" value="Western Australia" <?php echo $WA; ?>>Western Australia</option>
                                         </select>
-                                        <span class="required"><?php if(isset($reqState)){echo htmlentities($reqState); }?></span>
+                                        <span class="required"></span>
                                         <br/><label>State</label>
                                     </div>
                                     <div class="right-column">
-                                        <input type="text" name="Postcode-req-num" id="pcode" maxlength="50"/><span class="required"><?php if(isset($reqPostcode)){echo htmlentities($reqPostcode); }?></span>
+										<input type="text" name="Suburb-req-num" id="suburb" maxlength="50" value="<?php if(isset($_POST['Suburb-req-num'])) echo htmlentities($_POST['Suburb-req-num']);?>" /><span class="required"></span>
+                                        <br/><label>Suburb</label>
+																	
+                                        <br/><input type="text" name="Postcode-req-num" id="pcode" maxlength="50" value="<?php if(isset($_POST['Postcode-req-num'])) echo htmlentities($_POST['Postcode-req-num']);?>" /><span class="required"></span>
                                         <br/><label>Postcode</label>
                                     </div>
                                 </div>
